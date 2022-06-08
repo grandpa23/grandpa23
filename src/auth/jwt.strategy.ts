fc42993f1462e11bcdc486743099d442b2a13e72
vyacheslav.shuhlov <vyacheslav.shuhlov@gmail.com>
2022-06-08 14:32:26 +0300

    [+] stack

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: KEY,
+      secretOrKey: 'jwtSecretKey',
       ignoreExpiration: false,
     } as StrategyOptions);
   }
